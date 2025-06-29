6c1c67f083424996aca2bdddcab24710c7bf5eef
kdonrmadusanka <ravmkdon@gmail.com>
2025-06-29 12:56:37 +0530

    Update the Mammogram.routes.

--- routes/mammogram.routes.js
+++ routes/mammogram.routes.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
 const mammogramController = require("../controllers/mammogram.controller");
-
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
-});
-const upload = multer({ storage });
+const authMiddleware = require("../middleware/auth");
 
 router.post(
-  "/",
-  upload.single("mammogram"),
+  "/upload",
+  authMiddleware(["patient"]),
   mammogramController.uploadMammogram
 );
-router.get("/:id", mammogramController.getMammogramById);
-router.get("/patient/:patientId", mammogramController.getMammogramsByPatientId);
-router.delete("/:id", mammogramController.deleteMammogram);
+router.get(
+  "/:id",
+  authMiddleware(["patient", "doctor", "clinician"]),
+  mammogramController.getMammogramById
+);
+router.get(
+  "/patient/:patientId",
+  authMiddleware(["patient", "doctor", "clinician"]),
+  mammogramController.getMammogramsByPatientId
+);
+router.delete(
+  "/:id",
+  authMiddleware(["patient", "admin"]),
+  mammogramController.deleteMammogram
+);
 
 module.exports = router;
